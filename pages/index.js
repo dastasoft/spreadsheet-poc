cda6c88f057ee3aa624aff3f95b055b140f1fc44
dastasoft <dastasoft@gmail.com>
2020-09-14 18:05:32 +0200

    new api4

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,5 @@
-export default function Home({ data }) {
-  return (
-    <div className="p-4 mx-auto flex items-center">
-      <div
-        className={`grid grid-cols-${data.headers.length} gap-1 border-blue-800 border`}
-      >
-        {data.headers.map(header => (
-          <span className="font-bold text-lg border-blue-800 border  p-2">
-            {header}
-          </span>
-        ))}
-        {data.data.map(reg =>
-          reg.map(cell => (
-            <span className="p-2 border-blue-800 border">{cell}</span>
-          ))
-        )}
-      </div>
-    </div>
-  );
+export default function Home() {
+  return <div className="p-4 mx-auto flex items-center">Hola</div>;
 }
 
 // export async function getStaticProps() {
