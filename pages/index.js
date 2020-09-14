854fc2b885605f1d09835dd44c7ddba795e37270
dastasoft <dastasoft@gmail.com>
2020-09-14 18:06:56 +0200

    new api5

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,31 @@
-export default function Home() {
-  return <div className="p-4 mx-auto flex items-center">Hola</div>;
+export default function Home({ data }) {
+  return (
+    <div className="p-4 mx-auto flex items-center">
+      <div
+        className={`grid grid-cols-${data.headers.length} gap-1 border-blue-800 border`}
+      >
+        {data.headers.map(header => (
+          <span className="font-bold text-lg border-blue-800 border  p-2">
+            {header}
+          </span>
+        ))}
+        {data.data.map(reg =>
+          reg.map(cell => (
+            <span className="p-2 border-blue-800 border">{cell}</span>
+          ))
+        )}
+      </div>
+    </div>
+  );
 }
 
-// export async function getStaticProps() {
-//   const res = await fetch('https://spreadsheet-poc.vercel.app/api/sheet');
-//   const data = await res.json();
+export async function getStaticProps() {
+  const res = await fetch('https://spreadsheet-poc.vercel.app/api/sheet');
+  const data = await res.json();
 
-//   return {
-//     props: {
-//       data
-//     }
-//   };
-// }
+  return {
+    props: {
+      data
+    }
+  };
+}
