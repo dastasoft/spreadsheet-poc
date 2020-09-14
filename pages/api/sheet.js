37c7e4f1111f9d96d4f3088fa36959b1cfaedd08
dastasoft <dastasoft@gmail.com>
2020-09-14 17:55:35 +0200

    first commit

---
+++ pages/api/sheet.js
@@ -0,0 +1,29 @@
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+
+const googleSpreadSheet = async () => {
+  const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID);
+
+  await doc.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY
+  });
+
+  await doc.loadInfo();
+
+  const sheet = doc.sheetsByIndex[0];
+
+  const rows = await sheet.getRows();
+
+  return {
+    headers: rows[0]._sheet.headerValues,
+    data: rows.map(row => row._rawData)
+  };
+};
+
+export default async (req, res) => {
+  const data = await googleSpreadSheet();
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(data));
+};
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,31 @@
-import NextLogo from '../public/nextjs.svg';
-
-export default function Home() {
+export default function Home({ data }) {
   return (
     <div className="p-4 mx-auto flex items-center">
-      <NextLogo className="mr-4" width="100px" height="100px" />
-      <span>Boilerplate</span>
+      <div
+        className={`grid grid-cols-${data.headers.length} gap-1 border-blue-800 border`}
+      >
+        {data.headers.map(header => (
+          <span className="font-bold text-lg border-blue-800 border  p-2">
+            {header}
+          </span>
+        ))}
+        {data.data.map(reg =>
+          reg.map(cell => (
+            <span className="p-2 border-blue-800 border">{cell}</span>
+          ))
+        )}
+      </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:3000/api/sheet');
+  const data = await res.json();
+
+  return {
+    props: {
+      data
+    }
+  };
 }
